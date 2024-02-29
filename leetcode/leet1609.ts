df6ec21d3070bcf44980c980b0ab85a78bcc22d4
ja153903 <jaimeabbariao@gmail.com>
2024-02-29 00:42:00 -0500

    feat: solving the daily problem

---
+++ leetcode/leet1609.ts
@@ -0,0 +1,63 @@
+import type { TreeNode } from "../data-structures/trees";
+
+// This algorithms runs in O(n) time and space
+function isEvenOddTree(root: TreeNode | null): boolean {
+  if (!root) {
+    return false;
+  }
+
+  const nodes: number[][] = [];
+  const queue: TreeNode[] = [];
+
+  queue.push(root);
+
+  while (queue.length > 0) {
+    const size = queue.length;
+    const level: number[] = [];
+
+    for (let i = 0; i < size; i += 1) {
+      const front = queue.shift();
+      if (!front) {
+        throw new Error("Something went wrong with the test input");
+      }
+
+      level.push(front.val);
+
+      if (front.left) {
+        queue.push(front.left);
+      }
+
+      if (front.right) {
+        queue.push(front.right);
+      }
+    }
+
+    nodes.push(level);
+  }
+
+  for (let i = 0; i < nodes.length; i += 1) {
+    if (i % 2 === 0) {
+      for (let j = 0; j < nodes[i].length; j += 1) {
+        if (
+          nodes[i][j] % 2 === 0 ||
+          (j > 0 && nodes[i][j - 1] >= nodes[i][j])
+        ) {
+          return false;
+        }
+      }
+    } else {
+      for (let j = 0; j < nodes[i].length; j += 1) {
+        if (
+          nodes[i][j] % 2 === 1 ||
+          (j > 0 && nodes[i][j - 1] <= nodes[i][j])
+        ) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return true;
+}
+
+export { isEvenOddTree };
