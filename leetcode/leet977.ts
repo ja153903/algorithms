f8c9a9762d33fd50c2dede3ab3d4cb4f282c8b7d
ja153903 <jaimeabbariao@gmail.com>
2024-03-01 19:07:51 -0500

    feat: solving the daily problem

---
+++ leetcode/leet977.ts
@@ -0,0 +1,24 @@
+function sortedSquares(nums: number[]): number[] {
+  const result: number[] = new Array(nums.length);
+  let left = 0, right = nums.length - 1;
+  let index = result.length - 1;
+
+  while (left <= right) {
+    const leftSq = nums[left] * nums[left];
+    const rightSq = nums[right] * nums[right];
+
+    if (leftSq > rightSq) {
+      result[index] = leftSq;
+      left += 1;
+    } else {
+      result[index] = rightSq;
+      right -= 1;
+    }
+
+    index -= 1;
+  }
+
+  return result;
+}
+
+export { sortedSquares };
