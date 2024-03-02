66d40bac85e0b91fc2fe3c12f5cecac679bbffdd
ja153903 <jaimeabbariao@gmail.com>
2024-03-02 14:56:26 -0500

    feat: building binary heap priority queue

---
+++ data-structures/binary-heap.ts
@@ -0,0 +1,111 @@
+export type BinaryHeapComparator<T> = (a: T, b: T) => number;
+
+export class BinaryHeap<T> {
+  private container: T[];
+  private readonly comparator: BinaryHeapComparator<T>;
+
+  constructor(comparator: BinaryHeapComparator<T>) {
+    this.container = [];
+    this.comparator = comparator;
+  }
+
+  private getParentIndex(index: number): number {
+    return Math.floor((index - 1) / 2);
+  }
+
+  private getLeftChildIndex(index: number): number {
+    return 2 * index + 1;
+  }
+
+  private getRightChildIndex(index: number): number {
+    return 2 * index + 2;
+  }
+
+  private swap(left: number, right: number) {
+    [this.container[left], this.container[right]] = [
+      this.container[right],
+      this.container[left],
+    ];
+  }
+
+  private shiftUp(index: number) {
+    // NOTE: Suppose that the comparator is (a, b) => a - b
+    // Then what we're checking here is that if the parent is
+    // larger than the child, then we're swapping them around
+    // Since with this comparator, we want a min heap to be
+    // in front
+    while (
+      index > 0 &&
+
+      this.comparator(
+        this.container[this.getParentIndex(index)],
+        this.container[index],
+      ) > 0
+    ) {
+      this.swap(this.getParentIndex(index), index);
+      index = this.getParentIndex(index);
+    }
+  }
+
+  private shiftDown(index: number) {
+    let maxIndex = index;
+
+    // NOTE: Suppose that the comparator is (a, b) => a - b
+    // Then what we're checking here is if the left child
+    // is less than the current priority at the max index, then we want
+    // to make sure that we swap them if necessary
+    const leftChildIndex = this.getLeftChildIndex(index);
+    if (
+      leftChildIndex <= this.container.length - 1 &&
+      this.comparator(
+        this.container[leftChildIndex],
+        this.container[maxIndex],
+      ) < 0
+    ) {
+      maxIndex = leftChildIndex;
+    }
+
+    // NOTE: Suppose that the comparator is (a, b) => a - b
+    // Then what we're checking here is if the right child
+    // is less than the current priority at the max index, then we want
+    // to make sure that we swap them if necessary
+    const rightChildIndex = this.getRightChildIndex(index);
+    if (
+      rightChildIndex <= this.container.length - 1 &&
+      this.comparator(
+          this.container[rightChildIndex],
+          this.container[maxIndex]
+        ) < 0
+    ) {
+      maxIndex = rightChildIndex;
+    }
+
+    if (index !== maxIndex) {
+      this.swap(index, maxIndex);
+      this.shiftDown(maxIndex);
+    }
+  }
+
+  enqueue(item: T) {
+    this.container.push(item);
+    this.shiftUp(this.container.length - 1);
+  }
+
+  dequeue(): T | null {
+    if (this.size === 0) {
+      return null;
+    }
+
+    const result = this.container[0];
+    this.container[0] = this.container[this.size - 1];
+    this.container.pop();
+
+    this.shiftDown(0);
+
+    return result;
+  }
+
+  get size(): number {
+    return this.container.length;
+  }
+}
---
+++ data-structures/tests/binary-heap.test.ts
@@ -0,0 +1,69 @@
+import { expect, test } from "bun:test";
+
+import { BinaryHeap } from "../binary-heap";
+
+test("BinaryHeap - it should function as a MinHeap", () => {
+  const minHeap = new BinaryHeap((a: number, b: number) => a - b);
+
+  minHeap.enqueue(1);
+  minHeap.enqueue(2);
+  minHeap.enqueue(4);
+  minHeap.enqueue(3);
+
+  expect(minHeap.dequeue()).toBe(1);
+  expect(minHeap.dequeue()).toBe(2);
+  expect(minHeap.dequeue()).toBe(3);
+  expect(minHeap.dequeue()).toBe(4);
+  expect(minHeap.dequeue()).toBeNull();
+});
+
+test("BinaryHeap - it should function as a MinHeap with custom object priority", () => {
+  type CustomPriority = { value: number; priority: number };
+  const minHeap = new BinaryHeap(
+    (a: CustomPriority, b: CustomPriority) => a.priority - b.priority,
+  );
+
+  minHeap.enqueue({ value: 1, priority: 4 });
+  minHeap.enqueue({ value: 2, priority: 3 });
+  minHeap.enqueue({ value: 3, priority: 2 });
+  minHeap.enqueue({ value: 4, priority: 1 });
+
+  expect(minHeap.dequeue()).toEqual({ value: 4, priority: 1 });
+  expect(minHeap.dequeue()).toEqual({ value: 3, priority: 2 });
+  expect(minHeap.dequeue()).toEqual({ value: 2, priority: 3 });
+  expect(minHeap.dequeue()).toEqual({ value: 1, priority: 4 });
+  expect(minHeap.dequeue()).toBeNull();
+});
+
+test("BinaryHeap - it should function as a MaxHeap", () => {
+  const maxHeap = new BinaryHeap((a: number, b: number) => b - a);
+
+  maxHeap.enqueue(1);
+  maxHeap.enqueue(2);
+  maxHeap.enqueue(4);
+  maxHeap.enqueue(3);
+
+  expect(maxHeap.dequeue()).toBe(4);
+  expect(maxHeap.dequeue()).toBe(3);
+  expect(maxHeap.dequeue()).toBe(2);
+  expect(maxHeap.dequeue()).toBe(1);
+  expect(maxHeap.dequeue()).toBeNull();
+});
+
+test("BinaryHeap - it should function as a MaxHeap with custom object priority", () => {
+  type CustomPriority = { value: number; priority: number };
+  const maxHeap = new BinaryHeap(
+    (a: CustomPriority, b: CustomPriority) => b.priority - a.priority,
+  );
+
+  maxHeap.enqueue({ value: 1, priority: 4 });
+  maxHeap.enqueue({ value: 2, priority: 3 });
+  maxHeap.enqueue({ value: 3, priority: 2 });
+  maxHeap.enqueue({ value: 4, priority: 1 });
+
+  expect(maxHeap.dequeue()).toEqual({ value: 1, priority: 4 });
+  expect(maxHeap.dequeue()).toEqual({ value: 2, priority: 3 });
+  expect(maxHeap.dequeue()).toEqual({ value: 3, priority: 2 });
+  expect(maxHeap.dequeue()).toEqual({ value: 4, priority: 1 });
+  expect(maxHeap.dequeue()).toBeNull();
+});
