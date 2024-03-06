c18d49a700eb2dcc882a31ac9fbb0d3fdc74e422
ja153903 <jaimeabbariao@gmail.com>
2024-03-05 22:39:35 -0500

    feat: finishing up union find

--- data-structures/union-find.ts
+++ data-structures/union-find.ts
@@ -1,2 +1,38 @@
-// TODO: Build this out
-export class UnionFind {}
+export class UnionFind {
+  private rank: number[];
+  private parent: number[];
+
+  constructor(n: number) {
+    this.rank = new Array(n).fill(0);
+    this.parent = new Array(n);
+
+    for (let i = 0; i < n; i += 1) {
+      this.parent[i] = i;
+    }
+  }
+
+  union(u: number, v: number) {
+    const uParent = this.find(u);
+    const vParent = this.find(v);
+
+    if (uParent === vParent) {
+      return;
+    }
+
+    if (this.rank[uParent] < this.rank[vParent]) {
+      this.parent[uParent] = vParent;
+      this.rank[vParent] += 1;
+    } else {
+      this.parent[vParent] = uParent;
+      this.rank[uParent] += 1;
+    }
+  }
+
+  find(node: number): number {
+    if (this.parent[node] !== node) {
+      this.parent[node] = this.find(this.parent[node]);
+    }
+
+    return this.parent[node];
+  }
+}
