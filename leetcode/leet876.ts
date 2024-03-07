d0a30ac41e0666f8478debdddeef33af68bcf39c
ja153903 <jaimeabbariao@gmail.com>
2024-03-06 19:14:18 -0500

    feat: solving daily problem

---
+++ leetcode/leet876.ts
@@ -0,0 +1,15 @@
+import type { ListNode } from "@/data-structures/linked-lists";
+
+function middleNode(head: ListNode | null): ListNode | null {
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (fast?.next) {
+    slow = slow?.next ?? null;
+    fast = fast.next?.next ?? null;
+  }
+
+  return slow;
+}
+
+export { middleNode };
