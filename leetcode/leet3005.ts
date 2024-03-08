ee42f2427dfe4e417cfa3da1cd65c0af4f067317
ja153903 <jaimeabbariao@gmail.com>
2024-03-07 19:12:41 -0500

    feat: solving daily leetcode problem

---
+++ leetcode/leet3005.ts
@@ -0,0 +1,20 @@
+function maxFrequencyElements(nums: number[]): number {
+  const counter = new Map<number, number>();
+  nums.forEach((num) => {
+    counter.set(num, (counter.get(num) ?? 0) + 1);
+  });
+
+  const maxFrequency = Math.max(...counter.values());
+
+  let res = 0;
+
+  for (const value of counter.values()) {
+    if (value === maxFrequency) {
+      res += value;
+    }
+  }
+
+  return res;
+}
+
+export { maxFrequencyElements };
