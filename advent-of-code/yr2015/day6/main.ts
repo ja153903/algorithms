df2ea12cdaf91d2d81cf73987541a275974dc501
ja153903 <jaimeabbariao@gmail.com>
2024-03-08 01:10:40 -0500

    feat: solved two more aoc problems

---
+++ advent-of-code/yr2015/day6/main.ts
@@ -0,0 +1,146 @@
+import { parseByLine, readFile } from "@/advent-of-code/utils";
+
+const data = await readFile(import.meta.dir, "day6.in");
+
+type Coordinate = { row: number; col: number };
+
+type Action = "TURN_OFF" | "TURN_ON" | "TOGGLE";
+
+type Instruction = {
+  action: Action;
+  left: Coordinate;
+  right: Coordinate;
+};
+
+const TURN_OFF_REGEX =
+  /turn off (?<leftRow>\d+),(?<leftCol>\d+) through (?<rightRow>\d+),(?<rightCol>\d+)/gm;
+const TURN_ON_REGEX =
+  /turn on (?<leftRow>\d+),(?<leftCol>\d+) through (?<rightRow>\d+),(?<rightCol>\d+)/gm;
+const TOGGLE_REGEX =
+  /toggle (?<leftRow>\d+),(?<leftCol>\d+) through (?<rightRow>\d+),(?<rightCol>\d+)/gm;
+
+function buildInstruction(
+  match: RegExpMatchArray,
+  action: Action,
+): Instruction {
+  const leftRow = Number(match.groups?.leftRow ?? "");
+  const leftCol = Number(match.groups?.leftCol ?? "");
+  const rightRow = Number(match.groups?.rightRow ?? "");
+  const rightCol = Number(match.groups?.rightCol ?? "");
+
+  return {
+    action,
+    left: {
+      row: leftRow,
+      col: leftCol,
+    },
+    right: {
+      row: rightRow,
+      col: rightCol,
+    },
+  };
+}
+
+function matchInstruction(
+  line: string,
+  regex: RegExp,
+  action: Action,
+): Instruction {
+  for (const match of line.matchAll(regex)) {
+    return buildInstruction(match, action);
+  }
+
+  throw new Error("Something is wrong with the data");
+}
+
+const lines = parseByLine<Instruction>(data, (line: string) => {
+  if (line.startsWith("turn off")) {
+    return matchInstruction(line, TURN_OFF_REGEX, "TURN_OFF");
+  } else if (line.startsWith("turn on")) {
+    return matchInstruction(line, TURN_ON_REGEX, "TURN_ON");
+  } else {
+    return matchInstruction(line, TOGGLE_REGEX, "TOGGLE");
+  }
+});
+
+function buildStateGrid(): number[][] {
+  const state = new Array<Array<number>>(1000);
+
+  for (let i = 0; i < 1000; i += 1) {
+    state[i] = new Array<number>(1000).fill(0);
+  }
+
+  return state;
+}
+
+function applyAction(state: number[][], instruction: Instruction) {
+  const { action, left, right } = instruction;
+
+  for (let row = left.row; row <= right.row; row += 1) {
+    for (let col = left.col; col <= right.col; col += 1) {
+      switch (action) {
+        case "TOGGLE":
+          state[row][col] ^= 1;
+          break;
+        case "TURN_ON":
+          state[row][col] = 1;
+          break;
+        case "TURN_OFF":
+          state[row][col] = 0;
+          break;
+      }
+    }
+  }
+}
+
+function getLitCount(state: number[][]): number {
+  return state.reduce((acc, row) => acc + row.reduce((a, b) => a + b), 0);
+}
+
+function followInstructions(instructions: Instruction[]) {
+  const state = buildStateGrid();
+
+  for (const instruction of instructions) {
+    applyAction(state, instruction);
+  }
+
+  return getLitCount(state);
+}
+
+const part1 = followInstructions(lines);
+
+console.log(`AoC 2015 - Day 6 - Part 1: ${part1}`);
+
+function applyNewAction(state: number[][], instruction: Instruction) {
+  const { action, left, right } = instruction;
+
+  for (let row = left.row; row <= right.row; row += 1) {
+    for (let col = left.col; col <= right.col; col += 1) {
+      switch (action) {
+        case "TOGGLE":
+          state[row][col] += 2;
+          break;
+        case "TURN_ON":
+          state[row][col] += 1;
+          break;
+        case "TURN_OFF":
+          state[row][col] = Math.max(state[row][col] - 1, 0);
+          break;
+      }
+    }
+  }
+}
+
+function followNewInstructions(instructions: Instruction[]) {
+  const state = buildStateGrid();
+
+  for (const instruction of instructions) {
+    applyNewAction(state, instruction);
+  }
+
+  return getLitCount(state);
+}
+
+const part2 = followNewInstructions(lines);
+
+console.log(`AoC 2015 - Day 6 - Part 2: ${part2}`);
---
+++ advent-of-code/yr2015/day7/main.ts
@@ -0,0 +1,223 @@
+import { parseByLine, readFile } from "@/advent-of-code/utils";
+
+const data = await readFile(import.meta.dir, "day7.in");
+
+const ASSIGNMENT_REGEX = /(?<value>\w+) -> (?<register>\w+)/gm;
+const NEGATION_REGEX = /NOT (?<value>\w+) -> (?<register>\w+)/gm;
+const OPERATION_REGEX =
+  /(?<left>\w+) (?<operation>\w+) (?<right>\w+) -> (?<register>\w+)/gm;
+
+type Action = "ASSIGNMENT" | "NEGATION" | "AND" | "OR" | "LSHIFT" | "RSHIFT";
+type Assignment = {
+  action: "ASSIGNMENT";
+  register: string;
+  value: string;
+};
+type Negation = {
+  action: "NEGATION";
+  register: string;
+  value: string;
+};
+type Operation = {
+  action: "AND" | "OR" | "LSHIFT" | "RSHIFT";
+  register: string;
+  lhs: string;
+  rhs: string;
+};
+
+type Command = Assignment | Negation | Operation;
+
+function buildCommand(match: RegExpMatchArray, action: Action | null): Command {
+  const register = match.groups?.register ?? "";
+
+  switch (action) {
+    case "ASSIGNMENT":
+    case "NEGATION":
+      const value = match.groups?.value ?? "";
+      return { action, register, value };
+    default:
+      const operation = match.groups?.operation ?? "";
+      const lhs = match.groups?.left ?? "";
+      const rhs = match.groups?.right ?? "";
+
+      return { action: operation as Operation["action"], register, lhs, rhs };
+  }
+}
+
+function matchCommand(
+  command: string,
+  action: Action | null,
+  regex: RegExp,
+): Command {
+  for (const match of command.matchAll(regex)) {
+    return buildCommand(match, action);
+  }
+
+  throw new Error("This error cannot happen");
+}
+
+const lines = parseByLine<Command>(data, (line: string) => {
+  if (line.startsWith("NOT")) {
+    return matchCommand(line, "NEGATION", NEGATION_REGEX);
+  } else if (
+    line.includes("AND") ||
+    line.includes("OR") ||
+    line.includes("LSHIFT") ||
+    line.includes("RSHIFT")
+  ) {
+    return matchCommand(line, null, OPERATION_REGEX);
+  } else {
+    return matchCommand(line, "ASSIGNMENT", ASSIGNMENT_REGEX);
+  }
+});
+
+function setMap(map: Map<string, number>, commands: Command[]): boolean {
+  let noop = false;
+
+  for (const command of commands) {
+    const isRegisterSet = map.has(command.register);
+    if (isRegisterSet) {
+      continue;
+    }
+
+    noop = true;
+
+    switch (command.action) {
+      case "ASSIGNMENT":
+        const valueToAssign = parseInt(command.value);
+        if (isNaN(valueToAssign)) {
+          const ptr = map.get(command.value);
+          if (ptr != null) {
+            map.set(command.register, ptr);
+          }
+        } else {
+          map.set(command.register, valueToAssign);
+        }
+        break;
+      case "NEGATION":
+        const valueToNegate = parseInt(command.value);
+        if (isNaN(valueToNegate)) {
+          const ptr = map.get(command.value);
+          if (ptr != null) {
+            map.set(command.register, ~ptr);
+          }
+        } else {
+          map.set(command.register, ~valueToNegate);
+        }
+        break;
+      case "RSHIFT":
+        let lhsForRShift: number | undefined = parseInt(command.lhs);
+
+        if (isNaN(lhsForRShift)) {
+          lhsForRShift = map.get(command.lhs);
+          if (lhsForRShift == null) {
+            break;
+          }
+        }
+
+        let rhsForRShift: number | undefined = parseInt(command.rhs);
+
+        if (isNaN(rhsForRShift)) {
+          rhsForRShift = map.get(command.rhs);
+          if (rhsForRShift == null) {
+            break;
+          }
+        }
+
+        map.set(command.register, lhsForRShift >> rhsForRShift);
+        break;
+      case "LSHIFT":
+        let lhsForLShift: number | undefined = parseInt(command.lhs);
+
+        if (isNaN(lhsForLShift)) {
+          lhsForLShift = map.get(command.lhs);
+          if (lhsForLShift == null) {
+            break;
+          }
+        }
+
+        let rhsForLShift: number | undefined = parseInt(command.rhs);
+
+        if (isNaN(rhsForLShift)) {
+          rhsForLShift = map.get(command.rhs);
+          if (rhsForLShift == null) {
+            break;
+          }
+        }
+
+        map.set(command.register, lhsForLShift << rhsForLShift);
+        break;
+      case "AND":
+        let lhsForAnd: number | undefined = parseInt(command.lhs);
+
+        if (isNaN(lhsForAnd)) {
+          lhsForAnd = map.get(command.lhs);
+          if (lhsForAnd == null) {
+            break;
+          }
+        }
+
+        let rhsForAnd: number | undefined = parseInt(command.rhs);
+
+        if (isNaN(rhsForAnd)) {
+          rhsForAnd = map.get(command.rhs);
+          if (rhsForAnd == null) {
+            break;
+          }
+        }
+
+        map.set(command.register, lhsForAnd & rhsForAnd);
+        break;
+      case "OR":
+        let lhsForOr: number | undefined = parseInt(command.lhs);
+
+        if (isNaN(lhsForOr)) {
+          lhsForOr = map.get(command.lhs);
+          if (lhsForOr == null) {
+            break;
+          }
+        }
+
+        let rhsForOr: number | undefined = parseInt(command.rhs);
+
+        if (isNaN(rhsForOr)) {
+          rhsForOr = map.get(command.rhs);
+          if (rhsForOr == null) {
+            break;
+          }
+        }
+
+        map.set(command.register, lhsForOr | rhsForOr);
+        break;
+    }
+  }
+
+  return noop;
+}
+
+function fillMap(
+  commands: Command[],
+  overrides: [string, number][] = [],
+): number {
+  const map = new Map<string, number>();
+
+  for (const [key, value] of overrides) {
+    map.set(key, value);
+  }
+
+  let shouldRunLoop = true;
+
+  while (shouldRunLoop) {
+    shouldRunLoop = setMap(map, commands);
+  }
+
+  return map.get("a") ?? 0;
+}
+
+const part1 = fillMap(lines);
+
+console.log(`AoC 2015 - Day 7 - Part 1: ${part1}`);
+
+const part2 = fillMap(lines, [["b", 3176]]);
+
+console.log(`AoC 2015 - Day 7 - Part 2: ${part2}`);
