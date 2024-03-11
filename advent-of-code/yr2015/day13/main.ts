a0bc8645dbf4c2986fb4c01b439a7a87eb5f29f9
ja153903 <jaimeabbariao@gmail.com>
2024-03-11 17:40:11 -0400

    feat: started day 13 2015

--- advent-of-code/yr2015/day13/main.ts
+++ advent-of-code/yr2015/day13/main.ts
@@ -1,6 +1,32 @@
-import { TODO, readFile } from "@/advent-of-code/utils";
+import { TODO, parseByLine, readFile } from "@/advent-of-code/utils";
+
+type HappinessNode = {
+  src: string;
+  dst: string;
+  happiness: number;
+};
 
 const data = await readFile(import.meta.dir, "day13.in");
+
+const HAPPINESS_REGEX =
+  /(?<gains>\w+) would (?<gainOrLose>\w+) (?<happiness>\d+) happiness units by sitting next to (?<gainFrom>\w+)./gm;
+
+const lines = parseByLine<HappinessNode>(data, (line: string) => {
+  for (const match of line.matchAll(HAPPINESS_REGEX)) {
+    const dst = match.groups?.gains ?? "";
+    const src = match.groups?.gainFrom ?? "";
+    const gainOrLose = match.groups?.gainOrLose ?? "";
+    const happiness = Number(match.groups?.happiness ?? "");
+
+    return {
+      src,
+      dst,
+      happiness: gainOrLose === "gains" ? happiness : -happiness,
+    };
+  }
+
+  throw new Error("Something went wrong with the parsing");
+});
 
 const part1 = TODO<string>("Solve later");
 
