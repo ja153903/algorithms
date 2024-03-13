d42ca2c372050833c448f07b73aadd19f1ed6f18
ja153903 <jaimeabbariao@gmail.com>
2024-03-12 20:30:57 -0400

    feat: solved day 14

--- advent-of-code/yr2015/day14/main.ts
+++ advent-of-code/yr2015/day14/main.ts
@@ -1,4 +1,4 @@
-import { TODO, parseByLine, readFile } from "@/advent-of-code/utils";
+import { parseByLine, readFile } from "@/advent-of-code/utils";
 
 const data = await readFile(import.meta.dir, "day14.in");
 
@@ -25,10 +25,64 @@ const lines = parseByLine<Reindeer>(data, (line: string) => {
   throw new Error("Something went wrong with the regex");
 });
 
-const part1 = TODO<string>("Solve later");
+function getMetersFlown(reindeer: Reindeer, timeLimit: number): number {
+  const kilometerPerCycle = reindeer.speed * reindeer.flightTime;
+  const cycleTime = reindeer.flightTime + reindeer.restTime;
+
+  if (timeLimit <= cycleTime) {
+    return reindeer.speed * Math.min(timeLimit, reindeer.flightTime);
+  }
+
+  let result = 0;
+
+  const numCycles = Math.floor(timeLimit / cycleTime);
+
+  result += kilometerPerCycle * numCycles;
+
+  const remCycles = timeLimit % cycleTime;
+
+  if (remCycles > 0) {
+    result += reindeer.speed * Math.min(reindeer.flightTime, remCycles);
+  }
+
+  return result;
+}
+
+const part1 = Math.max(
+  ...lines.map((reindeer) => getMetersFlown(reindeer, 2503)),
+);
 
 console.log(`AoC 2015 - Day 14 - Part 1: ${part1}`);
 
-const part2 = TODO<string>("Solve later");
+function getLeaderboard(lines: Reindeer[], timeLimit: number) {
+  const map = new Map<string, number>();
+  for (const { name } of lines) {
+    map.set(name, 0);
+  }
+
+  // for each second, we update the score for each reindeer
+  for (let i = 1; i <= timeLimit; i += 1) {
+    // compute how far the reindeer has gone up to this point
+    let leaderDist = 0;
+    const leaderboard: [string, number][] = [];
+
+    for (const reindeer of lines) {
+      const metersFlown = getMetersFlown(reindeer, i);
+      leaderDist = Math.max(leaderDist, metersFlown);
+      leaderboard.push([reindeer.name, metersFlown]);
+    }
+
+    for (const [name, metersFlown] of leaderboard) {
+      if (metersFlown === leaderDist) {
+        map.set(name, (map.get(name) ?? 0) + 1);
+      }
+    }
+  }
+
+  return map;
+}
+
+const map = getLeaderboard(lines, 2503);
+const part2 = [...map.entries()].sort((a, b) => b[1] - a[1])[0][1];
 
 console.log(`AoC 2015 - Day 14 - Part 2: ${part2}`);
---
+++ advent-of-code/yr2015/day15/main.ts
@@ -0,0 +1,44 @@
+import { TODO, parseByLine, readFile } from "@/advent-of-code/utils";
+
+const data = await readFile(import.meta.dir, "day15.in");
+
+const INGREDIENTS_REGEX =
+  /(?<ingredient>\w+): capacity (?<capacity>\d+), durability (?<durability>\d+), flavor (?<flavor>\d+), texture (?<texture>\d+), calories (?<calories>\d+)/gm;
+
+type Ingredient = {
+  ingredient: string;
+  capacity: number;
+  durability: number;
+  flavor: number;
+  texture: number;
+  calories: number;
+};
+const ingredients = parseByLine<Ingredient>(data, (line: string) => {
+  for (const match of line.matchAll(INGREDIENTS_REGEX)) {
+    const ingredient = match.groups?.ingredient ?? "";
+    const capacity = Number(match.groups?.capacity ?? "");
+    const durability = Number(match.groups?.durability ?? "");
+    const flavor = Number(match.groups?.flavor ?? "");
+    const texture = Number(match.groups?.texture ?? "");
+    const calories = Number(match.groups?.calories ?? "");
+
+    return {
+      ingredient,
+      capacity,
+      durability,
+      flavor,
+      texture,
+      calories,
+    };
+  }
+
+  throw new Error("Something went wrong with parsing");
+});
+
+const part1 = TODO<string>("Solve later");
+
+console.log(`AoC 2015 - Day 15 - Part 1: ${part1}`);
+
+const part2 = TODO<string>("Solve later");
+
+console.log(`AoC 2015 - Day 15 - Part 2: ${part2}`);
