20c0bf2232c3441d4716c3161c34aba6a476acfd
ja153903 <jaimeabbariao@gmail.com>
2024-03-16 21:28:21 -0400

    feat: answered a question from biweekly contest

---
+++ leetcode/contests/biweekly-contest-136/q1.ts
@@ -0,0 +1,25 @@
+function sumOfEncryptedInt(nums: number[]): number {
+  const encrypt = (num: number): number => {
+    let len = 0;
+    let largest = 0;
+    let curr = num;
+
+    while (curr > 0) {
+      len += 1;
+      largest = Math.max(largest, curr % 10);
+      curr = Math.floor(curr / 10);
+    }
+
+    let res = 0;
+
+    for (let i = 0; i < len; i += 1) {
+      res = res * 10 + largest;
+    }
+
+    return res;
+  };
+
+  return nums.reduce((acc, num) => acc + encrypt(num), 0);
+}
+
+export { sumOfEncryptedInt };
