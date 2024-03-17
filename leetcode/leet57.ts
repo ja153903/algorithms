086b20ecbe4e51e4be8cf9cf8f7330dbad1bbb3c
ja153903 <jaimeabbariao@gmail.com>
2024-03-16 21:15:21 -0400

    feat: adding daily problem

---
+++ leetcode/leet57.ts
@@ -0,0 +1,31 @@
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+  // 1. find the intervals that are strictly less than the newInterval
+  // and add them to them to the result
+  // 2. handle all possible merges
+  // 3. insert new interval
+  // 4. insert the rest
+  const result: number[][] = [];
+  let i = 0;
+
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i]);
+    i += 1;
+  }
+
+  while (i < intervals.length && newInterval[1] >= intervals[i][0]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i += 1;
+  }
+
+  result.push(newInterval);
+
+  while (i < intervals.length) {
+    result.push(intervals[i]);
+    i += 1;
+  }
+
+  return result;
+}
+
+export { insert };
