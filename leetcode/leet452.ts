d3e34fa7dd99a16503128bfe02f01ba8b7180ffd
ja153903 <jaimeabbariao@gmail.com>
2024-03-17 21:34:58 -0400

    feat: adding scaffolding

---
+++ leetcode/leet452.ts
@@ -0,0 +1,8 @@
+import { TODO } from "@/advent-of-code/utils";
+
+function findMinArrowShots(points: number[][]): number {
+  // TODO: This is another interval question. Figure this out later
+  return TODO(0);
+}
+
+export { findMinArrowShots };
