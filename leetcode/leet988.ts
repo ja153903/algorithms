2d4846d39cb3864b630cac14366083d1a260894e
ja153903 <jaimeabbariao@gmail.com>
2024-04-18 22:42:53 -0400

    updating with daily problem from a while ago

--- leetcode/leet988.ts
+++ leetcode/leet988.ts
@@ -1,5 +1,34 @@
 import type { TreeNode } from "@/data-structures/trees"
 
-function smallestFromLeaf(root: TreeNode | null): string {}
+function smallestFromLeaf(root: TreeNode | null): string {
+  const paths: number[][] = []
+
+  function preOrder(node: TreeNode | null, path: number[]) {
+    if (!node) {
+      return
+    }
+
+    if (!node.left && !node.right) {
+      paths.push([...path, node.val])
+      return
+    }
+
+    preOrder(node.left, [...path, node.val])
+    preOrder(node.right, [...path, node.val])
+  }
+
+  preOrder(root, [])
+
+  return paths
+    .map((path) => convertPathToString(path))
+    .sort((a, b) => a.localeCompare(b))[0]
+}
+
+function convertPathToString(path: number[]): string {
+  return path
+    .reverse()
+    .map((ascii) => String.fromCharCode(ascii + 97))
+    .join("")
+}
 
 export { smallestFromLeaf }
