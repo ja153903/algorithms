a5751dcb784bd86987b6535f7bfba4e408393ad2
ja153903 <jaimeabbariao@gmail.com>
2024-04-27 23:28:30 -0400

    finally finishing day 15 yr 2015

--- advent-of-code/yr2015/day15/main.ts
+++ advent-of-code/yr2015/day15/main.ts
@@ -1,9 +1,9 @@
-import { TODO, parseByLine, readFile } from "@/advent-of-code/utils"
+import { parseByLine, readFile } from "@/advent-of-code/utils"
 
 const data = await readFile(import.meta.dir, "day15.in")
 
 const INGREDIENTS_REGEX =
-  /(?<ingredient>\w+): capacity (?<capacity>\d+), durability (?<durability>\d+), flavor (?<flavor>\d+), texture (?<texture>\d+), calories (?<calories>\d+)/gm
+  /(?<ingredient>\w+): capacity (?<capacity>-?\d+), durability (?<durability>-?\d+), flavor (?<flavor>-?\d+), texture (?<texture>-?\d+), calories (?<calories>-?\d+)/gm
 
 type Ingredient = {
   ingredient: string
@@ -33,13 +33,62 @@ const ingredients = parseByLine<Ingredient>(data, (line: string) => {
     }
   }
 
-  throw new Error("Something went wrong with parsing")
+  throw new Error("Something went wrong when capturing groups")
 })
 
-const part1 = TODO<string>("Solve later")
+function computeScore({
+  ingredients,
+  isPart1,
+}: {
+  ingredients: Ingredient[]
+  isPart1: boolean
+}): number {
+  let res = 0
+
+  // TODO: we want to find a solution for this such that
+  // we can find a general solution to generating n numbers
+  // that sum up to n (hard coding for current restraint)
+  for (let i = 1; i < 100; i += 1) {
+    for (let j = 1; j < 100; j += 1) {
+      for (let k = 1; k < 100; k += 1) {
+        for (let l = 1; l < 100; l += 1) {
+          if (i + j + k + l === 100) {
+            let calories = 0
+            let dict = { capacity: 0, durability: 0, flavor: 0, texture: 0 }
+            const ptrs = [i, j, k, l]
+
+            for (let ptr = 0; ptr < ptrs.length; ptr += 1) {
+              dict.capacity += ingredients[ptr].capacity * ptrs[ptr]
+              dict.durability += ingredients[ptr].durability * ptrs[ptr]
+              dict.flavor += ingredients[ptr].flavor * ptrs[ptr]
+              dict.texture += ingredients[ptr].texture * ptrs[ptr]
+
+              calories += ingredients[ptr].calories * ptrs[ptr]
+            }
+
+            if ((!isPart1 && calories === 500) || isPart1)
+              res = Math.max(
+                res,
+                Math.max(
+                  0,
+                  Object.values(dict)
+                    .map((value) => (value < 0 ? 0 : value))
+                    .reduce((a, b) => a * b, 1)
+                )
+              )
+          }
+        }
+      }
+    }
+  }
+
+  return res
+}
+
+const part1 = computeScore({ ingredients, isPart1: true })
 
 console.log(`AoC 2015 - Day 15 - Part 1: ${part1}`)
 
-const part2 = TODO<string>("Solve later")
+const part2 = computeScore({ ingredients, isPart1: false })
 
 console.log(`AoC 2015 - Day 15 - Part 2: ${part2}`)
