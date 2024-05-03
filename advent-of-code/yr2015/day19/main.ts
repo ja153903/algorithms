2e6e02841f92bf5e0d48a7a4d7daf6b4342bf635
ja153903 <jaimeabbariao@gmail.com>
2024-05-03 19:42:29 -0400

    updating with day 19 setup

---
+++ advent-of-code/yr2015/day19/main.ts
@@ -0,0 +1,11 @@
+import { TODO, readFile } from "@/advent-of-code/utils"
+
+const data = await readFile(import.meta.dir, "day19.in")
+
+const part1 = TODO<string>("Solve later")
+
+console.log(`AoC 2015 - Day 19 - Part 1: ${part1}`)
+
+const part2 = TODO<string>("Solve later")
+
+console.log(`AoC 2015 - Day 19 - Part 2: ${part2}`)
