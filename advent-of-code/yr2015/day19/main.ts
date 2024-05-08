518624a9efba8825ccba8a45de038e1e3a55480b
ja153903 <jaimeabbariao@gmail.com>
2024-05-07 23:40:36 -0400

    starting some work on this problem

--- advent-of-code/yr2015/day19/main.ts
+++ advent-of-code/yr2015/day19/main.ts
@@ -1,6 +1,21 @@
-import { TODO, readFile } from "@/advent-of-code/utils"
+import { TODO, parseByLine, readFile } from "@/advent-of-code/utils"
 
 const data = await readFile(import.meta.dir, "day19.in")
+
+type Entry = readonly [PropertyKey, string]
+
+const parsedData = parseByLine<unknown>(data, (line: string) => {
+  if (line.includes("=>")) {
+    return line.split(" => ")
+  }
+
+  return line
+})
+
+// TODO: These should not be entries in a record because each key
+// can have multiple mappings at some point
+const mapping = Object.fromEntries(parsedData.slice(0, -1) as Entry[])
+const sequence = (parsedData.at(-1) as string) ?? ""
 
 const part1 = TODO<string>("Solve later")
 
