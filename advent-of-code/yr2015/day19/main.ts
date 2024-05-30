2d52c85fd17ec77b98c17cce3a757982754d86c1
ja153903 <jaimeabbariao@gmail.com>
2024-05-30 18:48:34 -0400

    updating with more stuff

--- advent-of-code/yr2015/day19/main.ts
+++ advent-of-code/yr2015/day19/main.ts
@@ -38,39 +38,49 @@ console.log(`AoC 2015 - Day 19 - Part 1: ${molecules.size}`)
 // Currently it is too inefficient to start at e
 // Think about starting backwards until we get to e
 
-const queue: [string, number][] = [["e", 0]]
-const visited: Set<string> = new Set()
+function bfs() {
+  const queue: [string, number][] = [[start, 0]]
+  const visited: Set<string> = new Set()
 
-visited.add("e")
+  visited.add(start)
 
-while (queue.length > 0) {
-  const [current, steps] = queue.shift()!
-  if (current === start) {
-    console.log(`AoC 2015 - Day 19 - Part 2: ${steps}`)
-    break
-  }
+  while (queue.length > 0) {
+    const [current, steps] = queue.shift()!
+    if (current === "e") {
+      console.log(`AoC 2015 - Day 19 - Part 2: ${steps}`)
+      return
+    }
 
-  console.log(`Processing: ${current}`)
+    for (let i = 0; i < current.length; i += 1) {
+      for (let j = i; j < current.length; j += 1) {
+        const slice = current.slice(i, j + 1)
+        // given this substring, we want to check if there exists a mapping that we can use to replace the substring with
 
-  for (let i = 0; i < current.length; i += 1) {
-    for (let j = i; j < current.length; j += 1) {
-      const slice = current.slice(i, j + 1)
-      // given this substring, we want to check if there exists a mapping that we can use to replace the substring with
+        const relevantMappings = mappings.filter(
+          ([_key, value]) => value === slice
+        )
 
-      const relevantMappings = mappings.filter(([key, _value]) => key === slice)
+        // given all relevant mappings, then we should now replace the key with the value
+        // and bring it back to the string
 
-      // given all relevant mappings, then we should now replace the key with the value
-      // and bring it back to the string
+        for (const [key, _value] of relevantMappings) {
+          const replaced = `${current.slice(0, i)}${key}${current.slice(j + 1)}`
 
-      for (const [_key, value] of relevantMappings) {
-        const replaced = `${current.slice(0, i)}${value}${current.slice(j + 1)}`
-        if (visited.has(replaced)) {
-          continue
+          if (replaced === "e") {
+            console.log(`AoC 2015 - Day 19 - Part 2: ${steps}`)
+            return
+          }
+
+          if (visited.has(replaced)) {
+            continue
+          }
+
+          queue.push([replaced, steps + 1])
+          visited.add(replaced)
         }
-
-        queue.push([replaced, steps + 1])
-        visited.add(replaced)
       }
     }
   }
 }
+
+bfs()
